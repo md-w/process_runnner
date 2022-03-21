f4e6f4df5b8123a02dc1020f5a664ba2faa4b7a6
Monotosh Das <monotosh.das@videonetics.com>
2022-03-21 19:31:20 +0530

    WIP

---
+++ process_runner/src/process_runner_manager.cpp
@@ -0,0 +1,8 @@
+// *****************************************************
+//    Copyright 2022 Videonetics Technology Pvt Ltd
+// *****************************************************
+
+#include "process_runner_manager.h"
+ProcessRunnerManager::ProcessRunnerManager(/* args */) {}
+
+ProcessRunnerManager::~ProcessRunnerManager() {}
