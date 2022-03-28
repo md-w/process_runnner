d923df55965572f545f0406fbd43e79778e21feb
Monotosh Das <monotosh.das@videonetics.com>
2022-03-28 16:30:48 +0530

    docker build

--- process_runner_service/src/process_runner_service_run.cpp
+++ process_runner_service/src/process_runner_service_run.cpp
@@ -7,9 +7,11 @@
 #include "process_runner_service.h"
 
 ProcessRunnerServiceRun::ProcessRunnerServiceRun(std::string application_installation_directory,
-                                                 std::string config_directory, std::string data_directory)
+                                                 std::string config_directory, std::string data_directory,
+                                                 int listening_port)
     : _application_installation_directory(std::move(application_installation_directory)),
-      _config_directory(std::move(config_directory)), _data_directory(std::move(data_directory))
+      _config_directory(std::move(config_directory)), _data_directory(std::move(data_directory)),
+      _listening_port(listening_port)
 {
   _thread = std::make_unique<std::thread>(&ProcessRunnerServiceRun::run, this);
 }
@@ -33,7 +35,7 @@ void ProcessRunnerServiceRun::run()
 {
   RAY_LOG_INF << "Started";
   ::grpc::ServerBuilder builder;
-  builder.AddListeningPort("0.0.0.0:8787", ::grpc::InsecureServerCredentials());
+  builder.AddListeningPort(fmt::format("dns:///[::1]:{}", _listening_port), ::grpc::InsecureServerCredentials());
   ProcessRunnerService my_service(_application_installation_directory, _config_directory, _data_directory);
   builder.RegisterService(&my_service);
 
