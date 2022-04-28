a201fa98f3b4abea9c43e9e5cd8dd76455edc23d
Monotosh Das <monotosh.das@videonetics.com>
2022-04-28 04:30:08 +0000

    hash functino changed to absl hash

--- data_models/src/data_models_process_runner.cpp
+++ data_models/src/data_models_process_runner.cpp
@@ -2,18 +2,22 @@
 //    Copyright 2022 Videonetics Technology Pvt Ltd
 // *****************************************************
 
+#include <absl/hash/hash.h>
+
 #include "data_models_process_runner.h"
+
 namespace data_models
 {
 std::size_t hash_value(ProcessRunnerArguments const& s)
 {
-  std::size_t h1 = std::hash<std::string>{}(s.command);
+  // std::size_t h1 = std::hash<std::string>{}(s.command);
 
-  std::ostringstream os;
-  std::copy(s.args.begin(), s.args.end(), std::ostream_iterator<std::string>(os));
-  std::size_t h2 = std::hash<std::string>{}(os.str());
-  // NOLINTNEXTLINE(hicpp-signed-bitwise)
-  return h1 ^ (h2 << 1); // or use boost::hash_combine
+  // std::ostringstream os;
+  // std::copy(s.args.begin(), s.args.end(), std::ostream_iterator<std::string>(os));
+  // std::size_t h2 = std::hash<std::string>{}(os.str());
+  // // NOLINTNEXTLINE(hicpp-signed-bitwise)
+  // return h1 ^ (h2 << 1); // or use boost::hash_combine
+  return absl::HashOf(s.command, s.args);
 }
 
 } // namespace data_models
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -13,11 +13,11 @@ TEST_CASE("ProcessRunnerArguments", "[protobuf]")
 {
   data_models::ProcessRunnerArguments process_runner_arguments;
   process_runner_arguments.command = "/workspaces/vtpl_agent_2022/build/vtpl_agent/ffrecorder";
+  process_runner_arguments.args.emplace_back("-i");
+  process_runner_arguments.args.emplace_back("rtsp://admin:AdmiN1234@192.168.0.59/media/video1");
   process_runner_arguments.args.emplace_back("-o");
   process_runner_arguments.args.emplace_back(
       "/workspaces/vtpl_agent_2022/session/Data/4dc75/98c9a/recording_clip/major");
-  process_runner_arguments.args.emplace_back("-i");
-  process_runner_arguments.args.emplace_back("rtsp://admin:AdmiN1234@192.168.0.59/media/video1");
 
   data_models::ProcessRunnerArguments process_runner_arguments1;
   process_runner_arguments1.command = "/workspaces/vtpl_agent_2022/build/vtpl_agent/ffrecorder";
